ee714e19d7666ff3a6ddca35f84f78b837941f11
Kaige <millerkaige@gmail.com>
2022-09-19 19:05:02 -0700

    Game Controller

---
+++ FinalProjectTesting/Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinalProjectTesting;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace FinalProjectTesting.Controllers
+{
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        private CardGameContext _cardGameContext;
+        private readonly ILogger<GameController> _logger;
+        public GameController(ILogger<GameController> logger, CardGameContext cardGameContext)
+        {
+            _logger = logger;
+            _cardGameContext = cardGameContext;
+        }
+        
+        [Route("Test")]
+        [HttpGet]
+        public PromptCard[] Test()
+        {
+            var PromptCard = _cardGameContext.PromptCard.ToArray();
+            return PromptCard;
+        }
+
+
+    }
+}
